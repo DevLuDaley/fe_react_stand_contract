d73ee99a522e74b8c8cdd8a976521a31d864e4d8
devludaley <daleyluis@gmail.com>
2021-06-16 21:36:19 -0400

    Create TempPage.js

---
+++ src/user_auth/TempPage.js
@@ -0,0 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const TempPage = () => {
+  return (
+    <div>
+      {/* <Link to='/login'>Log In</Link>
+      <br></br>
+      <Link to='/signup'>Sign Up</Link> */
+      <h1> Just a Temp Page</h1>
+      
+      }
+    </div>
+  );
+};export default TempPage;
