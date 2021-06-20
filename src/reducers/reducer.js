7fe2a915ba522c8d8941170274ef820acaf59e29
devludaley <daleyluis@gmail.com>
2021-06-19 23:30:43 -0400

    Create reducer.js

---
+++ src/reducers/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import contractsReducer from './contractsReducer'
+
+const rootReducer = combineReducers({
+    ContractsReducer//, otherReducer
+})
+
+export default rootReducer
