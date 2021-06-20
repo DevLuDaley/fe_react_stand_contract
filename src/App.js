b4698d2a8aa0bb57bfaa3822ce04c51ef0b10b53
devludaley <daleyluis@gmail.com>
2021-06-19 21:49:13 -0400

    import axios && Home, Login, Signup, Contracts pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+// import {Home, Login, Logged_in} from './user_auth'
+import TempPage from './user_auth/TempPage'
+import Home from './user_auth/Home'
+import Login from './user_auth/Login'
+import Signup from './user_auth/Signup'
+import Contracts from './components/Contracts';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      isLoggedIn: false,
+      user: {}
+     };
+  }
+componentDidMount() {
+  this.loginStatus()
 }
+loginStatus = () => {
+    axios.get('http://localhost:3000/logged_in', 
+    {withCredentials: true})    
+.then(response => {
+      if (response.data.logged_in) {
+        this.handleLogin(response)
+      } else {
+        this.handleLogout()
+      }
+    })
+    .catch(error => console.log('api errors:', error))
+  }
+  
+handleLogin = (data) => {
+    this.setState({
+      isLoggedIn: true,
+      user: data.user
+    })
+  }
+handleLogout = () => {
+    this.setState({
+    isLoggedIn: false,
+    user: {}
+    })
+  }
+
+render() {
+    return (
+      <div>
+         <BrowserRouter>
+          <Switch>
+            <Route exact path='/' component={Home}
+
+            />
+            <Route exact path='/login' component={Login} 
+              // render={ handleLogin={handleLogin}} 
+
+            />
+            <Route exact path='/contracts' component={Contracts} 
+            />
+
+            <Route exact path='/TempPage' component={TempPage} 
+            />
+
+            <Route 
+              exact path='/signup' 
+              component={Signup} 
+/>
+
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
+  }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
