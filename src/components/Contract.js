d679b06bbeb8bd690733be6c75289eeb69eec4d6
devludaley <daleyluis@gmail.com>
2021-07-08 19:05:31 -0400

    Update Contract.js

--- src/components/Contract.js
+++ src/components/Contract.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 
 const Contract = (props) => {
