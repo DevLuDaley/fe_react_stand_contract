4028c69e8dddd7ba46903586f1c8824cfe768140
devludaley <daleyluis@gmail.com>
2021-09-22 15:41:25 -0400

    add {(renderProps)} to <Route>

--- src/App.js
+++ src/App.js
@@ -11,15 +11,17 @@ import Login from './user_auth/Login'
 import Signup from './user_auth/Signup'
 import Contract from './components/Contract';
 import ContractsList from './components/ContractsList';
+import ContractsPage from './components/ContractsPage';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      isLoggedIn: false,
-      // isLoggedIn: true,
-      user: {}
-     };
+    // this.state = { 
+    //   isLoggedIn: false,
+    //   // isLoggedIn: true,
+    //   user: {}
+    //  };
   }
 componentDidMount() {
   this.loginStatus()
@@ -30,6 +32,7 @@ loginStatus = () => {
 .then(response => {
       if (response.data.logged_in) {
         this.handleLogin(response)
+        console.log(" APP =>>> this.props ",this.props);
       } else {
         this.handleLogout()
       }
@@ -56,15 +59,32 @@ render() {
          <BrowserRouter>
           <Switch>
             <Route exact path='/' component={Home}
+            />
+            {/* <Route exact path='/login' component={Login}  */}
+              {/* // render={ handleLogin={handleLogin}}  */}
+            {/* /> */}
+             <Route
+                  exact path='/login'
+                  render={(routerProps) =>
+                      <Login {...routerProps} 
+                      contracts={this.props.contracts
+                      }
+              />}/>
 
-            />
-            <Route exact path='/login' component={Login} 
-              // render={ handleLogin={handleLogin}} 
-            />
             <Route exact path='/contract' component={Contract} 
             />
-            <Route exact path='/contractslist' component={ContractsList} 
+            <Route exact path='/contractslist' component={ContractsList}
             />
+
+            {/* <Route exact path='/contracts' component={ContractsPage} 
+            /> */}
+
+            <Route
+                            exact path='/contracts'
+                            render={(routerProps) =>
+                                <ContractsPage {...routerProps}
+                            contracts={this.props.contracts}
+                        />}/>
 
             <Route exact path='/TempPage' component={TempPage} 
             />
